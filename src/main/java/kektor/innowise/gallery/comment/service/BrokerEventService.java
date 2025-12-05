6086bc191fb3010ac1b8e02f431861b7cf547b75
kektor <alexvas06@gmail.com>
2025-12-05 16:17:23 +0300

    Added Retry logic in case of failed kafka event publication.

--- src/main/java/kektor/innowise/gallery/comment/service/BrokerEventService.java
+++ src/main/java/kektor/innowise/gallery/comment/service/BrokerEventService.java
@@ -1,10 +1,14 @@
 package kektor.innowise.gallery.comment.service;
 
 import kektor.innowise.gallery.comment.msg.CommentEventMessage;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
@@ -13,10 +17,13 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BrokerEventService {
 
-    final KafkaTemplate<Long, CommentEventMessage> kafkaTemplate;
+    RetryTemplate retryTemplate;
+    KafkaTemplate<Long, CommentEventMessage> kafkaTemplate;
 
+    @NonFinal
     @Value("${app.broker.comment-event-topic}")
     String commentEventTopicName;
 
@@ -26,11 +33,11 @@ public class BrokerEventService {
             phase = TransactionPhase.AFTER_COMMIT
     )
     public void newCommentEvent(CommentEventMessage event) {
-        kafkaTemplate.send(commentEventTopicName, event.imageId(), event)
-                .whenComplete((_, error) -> {
-                    if (error != null) {
-                        log.error("Error while sending comment event", error);
-                    }
+        retryTemplate.execute(_ -> kafkaTemplate.send(commentEventTopicName, event.imageId(), event).join(),
+                context -> {
+                    log.error("All retry attempts failed for sending comment event. CommentId: {} ImageId: {} UserId: {}",
+                            event.commentId(), event.imageId(), event.userId(), context.getLastThrowable());
+                    return null;
                 });
     }
 
